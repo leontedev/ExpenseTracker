b669bb8eb289eac3a7f65e10afa0abed0174e701
Mihail87 <leontemihai@gmail.com>
2019-10-29 15:05:10 +0200

    Day 36 completed

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,9 +8,67 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SecondView: View {
+    var name: String
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("Hello \(name)!")
+            Button("Dismiss") {
+                self.presentationMode.wrappedValue.dismiss()
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    // Sheets
+    @State private var showingSheet = false
+    
+    // ForEach .onDelete
+    @State var numbers = [Int]()
+    @State var currentNumber = 1
+    
+    // UserDefaults
+    @State var tapCount = UserDefaults.standard.integer(forKey: "Tap") // if not found, returns 0
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("\($0)")
+                    }.onDelete(perform: <#T##Optional<(IndexSet) -> Void>##Optional<(IndexSet) -> Void>##(IndexSet) -> Void#>)
+                }
+
+                Button("Add Number") {
+                    self.numbers.append(self.currentNumber)
+                    self.currentNumber += 1
+                }
+                
+                Spacer()
+                
+                Button("Tap count: \(tapCount)") {
+                    self.tapCount += 1
+                    UserDefaults.standard.set(self.tapCount, forKey: "Tap")
+                }
+                
+                Spacer()
+                
+                Button("Show Sheet, greet: Leonte") {
+                    self.showingSheet.toggle()
+                }
+                .sheet(isPresented: $showingSheet) {
+                    SecondView(name: "Leonte")
+                }
+            }.navigationBarItems(leading: EditButton())
+        }
+        
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
